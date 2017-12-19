6e381aaa46bcb94485a220c71e7bc3dc9a83adba
IndiraPD <34646827+IndiraPD@users.noreply.github.com>
2017-12-19 14:59:00 +0530

    Create leap year or not.c

---
+++ Code Kata/Beginner level/Set 1/leap year or not.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	scanf("%d",&a);
+	if(a%400==0)
+	{
+		printf("leap year");
+	}
+	else if(a%100==0)
+	{
+		printf("not a leap year");
+	}
+	else if(a%4==0)
+	{
+		printf("leap year");
+	}
+	else
+	{
+		printf("not a leap year");
+	}
+	return 0;
+}
