ddbdcf0032671c907993a5e81dbd09307fb05202
IndiraPD <34646827+IndiraPD@users.noreply.github.com>
2017-12-19 12:17:19 +0530

    Create operators.c

---
+++ Player level/operators.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a=5;
+int b=5;
+c=a+b;
+d=a-b;
+e=a*b;
+f=a/b;
+i=a%b;
+printf("%d",c);
+printf("%d",d);
+printf("%d",e);
+printf("%d",f);
+printf("%d",i);
+return 0;
+}
