00f19501250537fb0acd0e58b857c63d045621b3
IndiraPD <34646827+IndiraPD@users.noreply.github.com>
2018-06-03 11:35:43 +0530

    Create non_repeating.c

---
+++ Code Kata/Hunter level/Set 1/non_repeating.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	int n,arr[20],i,j,count;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		count=0;
+		for(j=i+1;j<n;j++)
+		{
+			if(arr[i]!=arr[j])
+			{
+				count+=1;
+			}
+		}
+			if(count==1)
+		{
+			printf("%d\t",arr[i]);
+		}
+	
+	}
+	
+
+	return 0;
+}
